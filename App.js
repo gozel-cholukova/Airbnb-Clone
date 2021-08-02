f6829d2a97f02af4ce16e0cf48ca1a26df0706a3
Gozel Cholukova <checkboxtm@gmail.com>
2021-08-03 02:13:19 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Header1 from './Header1';
+
+function App() {
+  return (
+
+    //BEM
+    <div className="app">
+      <Header />
+      <Header1 />
+      <Home />
+      {/*Home*/}
+        {/*Header*/}
+
+        {/*Banner*/}
+          {/*Search*/}
+
+        {/*Cards*/}
+
+        {/* Footer */}
+
+    {/*SearchPage*/}
+      {/*Header*/}
+      {/*...*/}
+      
+    </div>
+  );
+}
+
+export default App;
